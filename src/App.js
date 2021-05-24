bf5757d0b7b39d436b915f4feca2b2d1bec363c6
andrew-ballard1 <andrewj.ballard94@gmail.com>
2021-05-23 21:03:17 -0700

    Removing unused imports / functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React, { useEffect } from 'react';
-import BasicMap from './BasicMap';
 import Map from './Map'
 
 // ------------------------------------------------------------------
@@ -25,75 +24,24 @@ const droneOverviewData = [
 // data placeholders end
 // ------------------------------------------------------------------
 
-
-function LeftPane(){
-  const state = {
-    showPairs: true,
-    showTrains: false,
-    showInMotion: false,
-    showCharging: false,
-    showNeedsCharge: false
-  }
-
-  const buildDroneList = () => {
-    console.log('building drone list')
-
-    return droneOverviewData.map((item, index) => {
-      return <li key={item.id}>drone_{item.id}</li>
-    })
-  }
-
-
-  return (
-    <div className={'left_pane'}>
-      <div className={'flexCol_padding'}>
-        <h3>Filters</h3>
-        <div className={'flexCol'}>
-          <div className={'flexRow'}>
-            <div>Show Pairs</div>
-            <div><input type={'checkbox'} /></div>
-          </div>
-          <div className={'flexRow'}>
-            <div>Show Trains</div>
-            <div><input type={'checkbox'} /></div>
-          </div>
-          <div className={'flexRow'}>
-            <div>Show In Motion</div>
-            <div><input type={'checkbox'} /></div>
-          </div>
-          <div className={'flexRow'}>
-            <div>Show Warrant Requests</div>
-            <div><input type={'checkbox'} /></div>
-          </div>
-        </div>
-      </div>
-      <ul>
-        <li>Drones:</li>
-        {buildDroneList()}
-      </ul>
-    </div>
-  )
-}
-
-
 function App() {
 
   useEffect(() => {
     // POST request using fetch inside useEffect React hook
-    async function fetchData(){
-      const requestOptions = {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ title: 'React Hooks POST Request Example' })
-      };
-      const res = await fetch('https://reqres.in/api/posts', requestOptions)
-          .then(response => response.json())
-          .then(data => {
-            // console.log(data)
-            return data
-          });
-      console.log(res)
-    }
+    // async function fetchData(){
+    //   const requestOptions = {
+    //       method: 'POST',
+    //       headers: { 'Content-Type': 'application/json' },
+    //       body: JSON.stringify({ title: 'React Hooks POST Request Example' })
+    //   };
+    //   const res = await fetch('https://reqres.in/api/posts', requestOptions)
+    //       .then(response => response.json())
+    //       .then(data => {
+    //         // console.log(data)
+    //         return data
+    //       });
+    //   console.log(res)
+    // }
     // fetchData()
     // empty dependency array means this effect will only run once (like componentDidMount in classes)
   }, []);
